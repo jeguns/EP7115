c3da06c003c1d0b7ba463b55a7801ee4d87d2f74
Jesus Eduardo Gamboa Unsihuay <jgamboa@lamolina.edu.pe>
2021-06-14 20:40:22 -0500

    Actualización 14/06

--- 06 - Minimos cuadrados ponderados.R
+++ 06 - Minimos cuadrados ponderados.R
@@ -23,7 +23,7 @@ modelo0 %>% bptest()
 
 modelo0 %>% 
   augment %>% 
-  ggplot(aes(x=Dias,y=.resid))+
+    ggplot(aes(x=Dias,y=.resid))+
   geom_point(size = 3) + 
   geom_hline(yintercept=0)+
   labs(x = "Dias transcurridos",
@@ -37,33 +37,37 @@ modelo0 %>%
 
 # Caso 1:
 1 / lm(abs(modelo0$residuals) ~ modelo0$fitted.values)$fitted.values^2 -> peso1
-lm(Estudiantes ~ Dias, data = datos, weights = peso1) -> modelo1
+lm(Estudiantes ~ Dias, data = datos, weights = peso1) -> modeloA
 
-# Caso 2: var(e) = sigma2/dias 
+# Caso 2: var(e) = sigma2/dias = V, pero nosotros definimos W=V^-1 
 datos$Dias -> peso2
-lm(Estudiantes ~ Dias, data = datos, weights = peso2) -> modelo2
+lm(Estudiantes ~ Dias, data = datos, weights = peso2) -> modeloB
 
 # Comparacion -------------------------------------------------------------
 
 modelo0 %>% summary
-modelo1 %>% summary
-modelo2 %>% summary
+modeloA %>% summary
+modeloB %>% summary
 
 modelo0 %>% AIC
-modelo1 %>% AIC
-modelo2 %>% AIC
+modeloA %>% AIC
+modeloB %>% AIC
 
 modelo0 %>% BIC
-modelo1 %>% BIC
-modelo2 %>% BIC
+modeloA %>% BIC
+modeloB %>% BIC
 
 modelo0 %>% residuals %>% shapiro.test
-modelo1 %>% residuals %>% shapiro.test
-modelo2 %>% residuals %>% shapiro.test
+modeloA %>% residuals %>% shapiro.test
+modeloB %>% residuals %>% shapiro.test
 
 modelo0 %>% bptest()
-modelo1 %>% bptest()
-modelo2 %>% bptest()
+modeloA %>% bptest()
+modeloB %>% bptest()
+
+modelo0 %>% ols_test_breusch_pagan
+modeloA %>% ols_test_breusch_pagan
+modeloB %>% ols_test_breusch_pagan
 
 modelo0 %>% plot(which=1)
 modelo1 %>% plot(which=1)
@@ -73,10 +77,15 @@ modelo0 %>% plot(which=3)
 modelo1 %>% plot(which=3)
 modelo2 %>% plot(which=3)
 
+modelo0 %>% residuals %>% TSA::acf()
+modelo1 %>% residuals %>% TSA::acf()
+modelo2 %>% residuals %>% TSA::acf()
+
 data.frame(y = rstandard(modelo0),
            x = modelo1$fitted.values) %>%
   ggplot(aes(x = x, y = y)) + 
   geom_point() +
+  #geom_line()+
   geom_hline(yintercept = 0, linetype = "dotted") +
   labs(title = "Standardized Residuals vs Fitted Values Plot")
 
@@ -84,13 +93,14 @@ data.frame(y = rstandard(modelo1),
            x = modelo2$fitted.values) %>%
   ggplot(aes(x = x, y = y)) + 
   geom_point() +
+  #geom_line()+
   geom_hline(yintercept = 0, linetype = "dotted") +
   labs(title = "Standardized Residuals vs Fitted Values Plot")
 
 data.frame(y = rstandard(modelo2),
            x = modelo2$fitted.values) %>%
   ggplot(aes(x = x, y = y)) + 
-  geom_point() +
+  #geom_point() +
   geom_hline(yintercept = 0, linetype = "dotted") +
   labs(title = "Standardized Residuals vs Fitted Values Plot")
 
